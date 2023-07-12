1f39fe30485f5dc9d13a98e86f529a31142fc5ca
Vishal Jadhav <79803663+Vishal1711@users.noreply.github.com>
2023-07-12 00:29:44 -0400

    Update camFusion_Student.cpp

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -126,6 +126,7 @@ void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, 
     // display image
     string windowName = "3D Objects";
     cv::namedWindow(windowName, 1);
+    //cv::namedWindow(windowName, 2);
     cv::imshow(windowName, topviewImg);
 
     if(bWait)
@@ -135,10 +136,343 @@ void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, 
 }
 
 
+void nextClosestLidarPoints(std::vector<LidarPoint> &lidarPoints, vector<double> &closestPoints, int n)
+{
+    for(int i = 0; i < n; ++i)
+    {
+        double minX = 1e9;
+        for(auto it = lidarPoints.begin(); it != lidarPoints.end(); ++it)
+        {
+            minX = ((minX > it->x) && (it->x > closestPoints[i])) ? it->x : minX;
+        }
+        closestPoints.push_back(minX);
+    }
+}
+
+void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
+                     std::vector<LidarPoint> &lidarPointsCurr, double frameRate, double &TTC)
+{
+    // For estimating TTC, we need the closest reliable LiDAR point in both previous and current point clouds
+    vector<double> closestPointsPrev, closestPointsCurr;  //for robust estimation to avoid considering outliers
+    
+    //2 Approches implemented - 1) considering 5 closest points. 2) considering all LiDAR points
+    /* Approach 1 Begin - considering 5 closest points */ //Line 161-178
+    // Finding closest distance in both previous and current lidar points
+    double minXCurr = 1e9, minXPrev = 1e9;
+    for(auto it = lidarPointsPrev.begin(); it != lidarPointsPrev.end(); ++it)
+    {
+        minXPrev = minXPrev > it->x ? it->x : minXPrev;
+    }
+
+    for(auto it = lidarPointsCurr.begin(); it != lidarPointsCurr.end(); ++it)
+    {
+        minXCurr = minXCurr > it->x ? it->x : minXCurr;
+    }
+
+    closestPointsPrev.push_back(minXPrev);
+    closestPointsCurr.push_back(minXCurr);
+
+    //minXPrev and minXCurr could be reliable closest LiDAR points or unwanted outliers
+    //Finding next 4 closest points in both previous and current frames
+    nextClosestLidarPoints(lidarPointsPrev, closestPointsPrev, 4); //by changing parameter 4 here, we can find as many closest points as required
+    nextClosestLidarPoints(lidarPointsCurr, closestPointsCurr, 4);
+    //Now we have 5 closest points in both previous and current frames
+    /* Approach 1 end - considering 5 closest points */
+
+    /* Approach 2 Begin - considering all LiDAR points */ //Line - 183-193
+    /**for(auto it = lidarPointsPrev.begin(); it != lidarPointsPrev.end(); ++it)
+    {
+        closestPointsPrev.push_back(it->x);
+    }
+
+    for(auto it = lidarPointsCurr.begin(); it != lidarPointsCurr.end(); ++it)
+    {
+        closestPointsCurr.push_back(it->x);
+    }
+    sort(closestPointsPrev.begin(), closestPointsPrev.end());
+    sort(closestPointsCurr.begin(), closestPointsCurr.end());**/
+    //Now we have all lidar point distances sorted
+    /* Approach 2 end - considering all LiDAR points */
+
+    /* to check distance between closest LiDAR points to identify outliers based on distance thresold */
+  	// distance thresold - if distance between 2 successive closest LiDAR points is greater than distance thresold then the closest point is an outlier
+    /*cout << "closestPointsPrev ";
+    for(int i = 0; i < 5 ; ++i){
+        cout << "- " << i << ") " <<
+        closestPointsPrev[i] << " ";
+    }
+    cout << endl;
+
+    cout << "closestPointsCurr ";
+    for(int i = 0; i < 5 ; ++i){
+        cout << "- " << i << ") " <<
+        closestPointsCurr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Dist btwn Prev closest Points ";
+    for(int i = 0; i < 4 ; ++i){
+        cout << "- " << i << ") " <<
+        closestPointsPrev[i+1] - closestPointsPrev[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Dist btwn Curr closest Points ";
+    for(int i = 0; i < 4 ; ++i){
+        cout << "- " << i << ") " <<
+        closestPointsCurr[i+1] - closestPointsCurr[i] << " ";
+    }
+    cout << endl;*/
+    /* trial */
+
+    //checking which closest point to consider.
+    //Threshold distance between 2 reliable closest points in a Point cloud is taken as 0.01m
+    double closestXPrev, closestXCurr;
+    
+    /* Approach 1 begin */ //Inefficient approach - improved below
+    /*if((closestPointsPrev[1] - closestPointsPrev[0]) < 0.03 && (closestPointsPrev[2] - closestPointsPrev[1])  < 0.03 &&
+        (closestPointsPrev[3] - closestPointsPrev[2]) < 0.03)
+    {
+        closestXPrev = closestPointsPrev[0];
+    } else if((closestPointsPrev[2] - closestPointsPrev[1]) < 0.03 && (closestPointsPrev[3] - closestPointsPrev[2])  < 0.03 &&
+              (closestPointsPrev[4] - closestPointsPrev[3]) < 0.03)
+    {
+        closestXPrev = closestPointsPrev[1];
+    }
+
+    if((closestPointsCurr[1] - closestPointsCurr[0]) < 0.03 && (closestPointsCurr[2] - closestPointsCurr[1])  < 0.03 &&
+        (closestPointsCurr[3] - closestPointsCurr[2]) < 0.03)
+    {
+        closestXCurr = closestPointsCurr[0];
+    } else if((closestPointsCurr[2] - closestPointsCurr[1]) < 0.03 && (closestPointsCurr[3] - closestPointsCurr[2])  < 0.03 &&
+              (closestPointsCurr[4] - closestPointsCurr[3]) < 0.03)
+    {
+        closestXCurr = closestPointsCurr[1];
+    }*/
+    /* Approach 1 end */
+
+    /* Improvization 1 begin */
+    /*for(size_t i = 0; i < closestPointsPrev.size() - 3; ++i)
+    {
+        if((closestPointsPrev[i+1] - closestPointsPrev[i]) < 0.03 && (closestPointsPrev[i+2] - closestPointsPrev[i+1])  < 0.03 &&
+            (closestPointsPrev[i+3] - closestPointsPrev[i+2]) < 0.03)
+        {
+            closestXPrev = closestPointsPrev[i];
+            break;
+        } 
+    }
+
+    for(size_t i = 0; i < closestPointsCurr.size() - 3; ++i)
+    {
+        if((closestPointsCurr[i+1] - closestPointsCurr[i]) < 0.03 && (closestPointsCurr[i+2] - closestPointsCurr[i+1])  < 0.03 &&
+            (closestPointsCurr[i+3] - closestPointsCurr[i+2]) < 0.03)
+        {
+            closestXCurr = closestPointsCurr[i];
+            break;
+        } 
+    }*/
+    /* Improvization 1 end */
+
+    /* Improvization 2 begin */ //refactoring above improvisation by implementing bitwise operators
+    for(size_t i = 0; i < closestPointsPrev.size() - 3; ++i)
+    {
+        bool check = true;
+        for(int j = 0; j < 3; ++j)
+        {
+            check &= (closestPointsPrev[i+j+1] - closestPointsPrev[i+j]) < 0.01;
+        }
+        if(check)
+        {
+            closestXPrev = closestPointsPrev[i];
+            break;
+        } 
+    }
+
+    for(size_t i = 0; i < closestPointsCurr.size() - 3; ++i)
+    {
+        bool check = true;
+        for(int j = 0; j < 3; ++j)
+        {
+            check &= (closestPointsCurr[i+j+1] - closestPointsCurr[i+j]) < 0.01;
+        }
+        if(check)
+        {
+            closestXCurr = closestPointsCurr[i];
+            break;
+        } 
+    }
+    /* Improvization 2 end */
+    
+    //TTC computation
+    double dT = 1 / frameRate;
+    //TTC = minXCurr * dT / (minXPrev - minXCurr);
+    TTC = closestXCurr * dT / (closestXPrev - closestXCurr);
+
+}
+
+
+// associating bounding boxes between previous and current frame using keypoint matches
+void matchBoundingBoxes(std::vector<cv::DMatch> &matches, std::map<int, int> &bbBestMatches, DataFrame &prevFrame, DataFrame &currFrame)
+{
+    //multimap container for potential match candidates
+    std::multimap<int, int> potentialBBMatches;
+
+    //loop over keypoint matches
+    for(std::vector<cv::DMatch>::iterator it1 = matches.begin(); it1 != matches.end(); ++it1)
+    {
+        // extracting matched keypoints from prev and curr frames
+        cv::KeyPoint KptCurr = currFrame.keypoints.at(it1->trainIdx);
+        cv::KeyPoint KptPrev = prevFrame.keypoints.at(it1->queryIdx);
+
+        //checking which bounding boxes enclose these keypoints
+
+        //to check if keypoint is enclosed in more than 1 bounding box
+        vector<vector<BoundingBox>::iterator> KptCurrEnclosingBoxes, KptPrevEnclosingBoxes;
+
+        //looping over Bounding boxes in current frame
+        std::vector<BoundingBox> currFrameBBoxes = currFrame.boundingBoxes;
+        
+        //for(std::vector<BoundingBox>::iterator it2 = currFrameBBoxes.begin(); it2 != currFrameBBoxes.end(); ++it2)
+        for(auto it2 = currFrameBBoxes.begin(); it2 != currFrameBBoxes.end(); ++it2)  //same as above line
+        {
+            if(it2->roi.contains(KptCurr.pt))
+            {
+                KptCurrEnclosingBoxes.push_back(it2);
+            }
+        }
+
+        //looping over Bounding boxes in previous frame
+        std::vector<BoundingBox> prevFrameBBoxes = prevFrame.boundingBoxes;
+        for(auto it3 = prevFrameBBoxes.begin(); it3 != prevFrameBBoxes.end(); ++it3)
+        {
+            if(it3->roi.contains(KptPrev.pt))
+            {
+                KptPrevEnclosingBoxes.push_back(it3);
+            }
+        }
+
+        //checking and ignoring if matched keypoint in either frame is enclosed by more than 1 bounding box or no bouding box
+        if((KptCurrEnclosingBoxes.size() == 1) && (KptPrevEnclosingBoxes.size() == 1)) 
+        {
+            int prevBboxID = KptPrevEnclosingBoxes[0]->boxID;
+            int currBboxID = KptCurrEnclosingBoxes[0]->boxID;
+            
+            //adding potential match candidates into a multimap container
+            potentialBBMatches.insert(std::pair<int, int> (prevBboxID, currBboxID));
+        }
+    }
+
+    ////checking for match candidates with highest number of occurences
+    int prevBBoxesCount = prevFrame.boundingBoxes.size();
+
+    //bounding box IDs in any DataFrame starts from 0
+    for(size_t i = 0; i < prevBBoxesCount; ++i)
+    {
+        //equal_range() function returns a pair of iterators
+        std::pair<multimap<int, int>::iterator, multimap<int, int>::iterator> it = potentialBBMatches.equal_range(i);
+        vector<int> currBBIds; // Bounding boxes in current frame mapped to previous bounding box with boxID = i
+        
+        //using equal_range(i) to get potential matches of previous frame bouding box with ID = i. 
+        //Bounding box IDs in any DataFrame starts from 0
+        //if value 'i' is not present as key in multimap, then below for loop will not run, then currBBIds.size() = 0
+        for(multimap<int, int>::iterator it1 = it.first; it1 != it.second; ++it1)
+        {
+            //collecting all mapped values to key 'i'
+            currBBIds.push_back(it1->second);
+        }
+
+        if(currBBIds.size() > 0){ /// to avoid segmentation fault error for using 'currBBIds[0]' if currBBIds.size() = 0
+            
+            //finding value with highest number of occurences
+            sort(currBBIds.begin(), currBBIds.end());
+            int maxCount = 1, element = currBBIds[0], count = 1;
+            for(int j = 1; j < currBBIds.size(); ++j)
+            {
+                if(currBBIds[j] == currBBIds[j-1]) {
+                    count++;
+                } else {
+                    if(count > maxCount){
+                        maxCount = count;
+                        element = currBBIds[j-1];
+                    }
+                    count = 1;
+                }
+            }
+            //if last element is the most frequent element
+            if(count > maxCount){
+                maxCount = count;
+                element = currBBIds[currBBIds.size()-1];
+            }
+
+            // 'element' is the variable that holds highest occuring matching current frame bounding box ID
+            //inserting best matches
+            bbBestMatches.insert(pair<int, int>(i, element));
+        }
+    }
+}
+
+
 // associate a given bounding box with the keypoints it contains
 void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, std::vector<cv::DMatch> &kptMatches)
 {
-    // ...
+    vector<cv::DMatch> enclosedMatches;  ///Keypoint matches enclosed by region of interest of bounding box.
+
+    for(auto it = kptMatches.begin(); it != kptMatches.end(); ++it)
+    {
+        cv::KeyPoint kptCurr = kptsCurr[it->trainIdx];
+        if(boundingBox.roi.contains(kptCurr.pt))
+        {
+            enclosedMatches.push_back(*it);
+        }
+    }
+
+    //Problem is that there will be outliers among enclosedMatches
+    //solution - computing mean of all euclidian distances between keypoint matches and then remove those that are too far away from the mean
+    vector<double> distances;
+    for(auto it = enclosedMatches.begin(); it != enclosedMatches.end(); ++it)
+    {
+        cv::KeyPoint kptCurr = kptsCurr[it->trainIdx];
+        cv::KeyPoint kptPrev = kptsPrev[it->queryIdx];
+
+        //euclidian distance between matched keypoints
+        double eucDist = cv::norm(kptCurr.pt - kptPrev.pt);
+        distances.push_back(eucDist);
+    }
+
+    //computing mean of all distances
+    double meanDist = std::accumulate(distances.begin(), distances.end(), 0.0) / distances.size();
+
+    //cout << "------------------------------------------" << endl;
+    //cout << "size of distances vector - " << distances.size() << endl;
+    //cout << "mean of disatnces - " << meanDist << endl;
+    //sort(distances.begin(), distances.end());
+
+    /**for(size_t i = 0; i < distances.size(); ++i)
+    {
+        cout << i+1 << ") " << distances[i] << " - ";
+    }
+    cout << endl;**/
+
+    //filtering collected keypoint matches based on euclidian distance between keypoint matches
+    //filtering criterion is - euclidian distance between keypoint matches must fall between 1 and 3 times mean distance(3*meanDist). Decided by observing data.
+    for(auto it = enclosedMatches.begin(); it != enclosedMatches.end(); ++it)
+    {
+        cv::KeyPoint kptCurr = kptsCurr[it->trainIdx];
+        cv::KeyPoint kptPrev = kptsPrev[it->queryIdx];
+
+        //euclidian distance between matched keypoints
+        double eucDist = cv::norm(kptCurr.pt - kptPrev.pt);
+        
+        if(!((eucDist >= 1) && (eucDist < (3*meanDist))))
+        {
+            enclosedMatches.erase(it);
+            it--;
+        }
+    }
+
+    //adding filtered keypoint correspondences to the respective bounding box
+    boundingBox.kptMatches = enclosedMatches;
+
 }
 
 
@@ -146,18 +480,56 @@ void clusterKptMatchesWithROI(BoundingBox &boundingBox, std::vector<cv::KeyPoint
 void computeTTCCamera(std::vector<cv::KeyPoint> &kptsPrev, std::vector<cv::KeyPoint> &kptsCurr, 
                       std::vector<cv::DMatch> kptMatches, double frameRate, double &TTC, cv::Mat *visImg)
 {
-    // ...
-}
+    // compute distance ratios between all matched keypoints
+    vector<double> distRatios; // stores the distance ratios for all keypoints between curr. and prev. frame
+    for (auto it1 = kptMatches.begin(); it1 != kptMatches.end() - 1; ++it1)
+    { // outer keypoint loop
 
+        // get current keypoint and its matched partner in the prev. frame
+        cv::KeyPoint kpOuterCurr = kptsCurr.at(it1->trainIdx);
+        cv::KeyPoint kpOuterPrev = kptsPrev.at(it1->queryIdx);
 
-void computeTTCLidar(std::vector<LidarPoint> &lidarPointsPrev,
-                     std::vector<LidarPoint> &lidarPointsCurr, double frameRate, double &TTC)
-{
-    // ...
-}
+        for (auto it2 = kptMatches.begin() + 1; it2 != kptMatches.end(); ++it2)
+        { // inner keypoint loop
 
+            double minDist = 100.0; // min. required distance
 
-void matchBoundingBoxes(std::vector<cv::DMatch> &matches, std::map<int, int> &bbBestMatches, DataFrame &prevFrame, DataFrame &currFrame)
-{
-    // ...
+            // get next keypoint and its matched partner in the prev. frame
+            cv::KeyPoint kpInnerCurr = kptsCurr.at(it2->trainIdx);
+            cv::KeyPoint kpInnerPrev = kptsPrev.at(it2->queryIdx);
+
+            double distCurr = cv::norm(kpOuterCurr.pt - kpInnerCurr.pt);
+            double distPrev = cv::norm(kpOuterPrev.pt - kpInnerPrev.pt);
+
+            if (distPrev > std::numeric_limits<double>::epsilon() && distCurr >= minDist)
+            { // avoid division by zero
+
+                double distRatio = distCurr / distPrev;
+                distRatios.push_back(distRatio);
+            }
+        } // eof inner loop over all matched kpts
+    }     // eof outer loop over all matched kpts
+
+    // only continue if list of distance ratios is not empty
+    if (distRatios.size() == 0)
+    {
+        TTC = NAN;
+        return;
+    }
+
+    //First sorting in  ascending order to finding the median
+    sort(distRatios.begin(),distRatios.end());
+    double medianDistRatio;
+
+    //Finding median by considering both even and odd number of elements cases
+    int numOfDistCalc = distRatios.size();
+    if(numOfDistCalc%2 != 0){
+       medianDistRatio = distRatios.at((numOfDistCalc-1)/2);
+    } else {
+        medianDistRatio = (distRatios.at((numOfDistCalc/2)-1) + distRatios.at(numOfDistCalc/2))/2;
+    }
+
+    double dT = 1 / frameRate;
+    TTC = -dT / (1 - medianDistRatio);
+
 }
